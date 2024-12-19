4108d0dbe5980f3b2e5580a92a94f40fbb33e376
Mickey_zhaang <qzhang27@colby.edu>
2024-12-18 23:45:44 -0500

    Fixed all issues

---
+++ src/constant.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `hello pc`,
+    "cs-degree": `hello pain and suffering`,
+    "sofa-table": `hello sofa`,
+    tv: `hello tv`,
+    bed: `hello bved`,
+    resume: `hello resume`,
+    projects: `hello projects`,
+    library: `hello library`,
+    exit: "hello exit",
+}
