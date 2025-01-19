e449590175f5b38f5f599643d5f9c4b6817beb81
Mickey_zhaang <qzhang27@colby.edu>
2025-01-19 10:39:50 -0500

    modularized movement functions to reduce amount of code written in k.scene()

--- src/js/utils.js
+++ src/js/utils.js
@@ -31,3 +31,65 @@ export function setCamScale(k) {
     k.camScale(k.vec2(1.5));
   }
 }
+
+export function mouseMovement(k, player, mouseBtn) {
+  if (mouseBtn !== "left" || player.isInDialogue) return;
+
+    const worldMousePos = k.toWorld(k.mousePos());
+    player.moveTo(worldMousePos, player.speed);
+
+    const mouseAngle = player.pos.angle(worldMousePos);
+
+    const lowerBound = 50;
+    const upperBound = 125;
+
+    if (
+      mouseAngle > lowerBound &&
+      mouseAngle < upperBound &&
+      player.curAnim() !== "walk-up"
+    ) {
+      player.play("walk-up");
+      player.direction = "up";
+      return;
+    }
+
+    if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim() !== "walk-down"
+    ) {
+      player.play("walk-down");
+      player.direction = "down";
+      return;
+    }
+
+    if (Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "right";
+      return;
+    }
+
+    if (Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "left";
+      return;
+    }
+
+    //Stopping Animation
+    function stopAnims() {
+      if (player.direction === "down") {
+        player.play("idle-down");
+        return;
+      }
+      if (player.direction === "up") {
+        player.play("idle-up");
+        return;
+      }
+    
+      player.play("idle-side");
+    }
+    k.onMouseRelease(stopAnims)
+  
+}
