0a685cfa846fdeaa3cd79ea4048074b7a50c9e62
Mickey_zhaang <qzhang27@colby.edu>
2025-01-30 18:25:06 -0500

    testing

--- src/js/debuggingGameUtils/index.js
+++
@@ -1,9 +0,0 @@
-export { playerInteractingWith } from "./playerInteraction";
-export { interactionRange, scaleFactor } from "./constants";
-export { handleLayers } from "./handlingLayers";
-export { k } from "./kaboomCtx";
-export { settingUpCameraMovement } from "./settingUpCamera";
-export { mouseMovement } from "./movement";
-export { createPlayer } from "./createPlayer";
-export { displayDialogue } from "./displayDialogue";
-export { loadAssets } from "./loadAssets";
